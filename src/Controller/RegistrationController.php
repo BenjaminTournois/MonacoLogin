be3bbcecd9272e3c86c8f84e67059a3109877381
BenjaminTourois <tournoisbenjamin@gmail.com>
2019-05-15 07:32:49 +0200

    First commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Controller;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends Controller {
+
+    /**
+     * @Route("/register")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $user->setIsActive(true);
+
+            //Uncomment the line below to create an Admin Account
+            //$user->addRole("ROLE_ADMIN");
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            
+            $this->addFlash('success', 'Votre compte à bien été enregistré.');
+            return $this->redirectToRoute('login');
+        }
+        return $this->render('registration/register.html.twig', ['form' => $form->createView(), 'mainNavRegistration' => true, 'title' => 'Inscription']);
+    }
+
+}
